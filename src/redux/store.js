565253ddf2b72441c2053438d1653b170e7e8109
srabon <srabon.gems@mopa.gov.bd>
2024-02-11 19:50:39 +0600

    renm

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,9 @@
 import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from '@redux-devtools/extension';
-import thunk from 'redux-thunk'
-import cartCounter from "./middleware/cartCounter";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(cartCounter,thunk))
-  );
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 export default store;
