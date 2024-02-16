1dfbcae2b724bc826e756ecc01a0c8ac5027f9e9
srabon <srabon.gems@mopa.gov.bd>
2024-02-16 11:31:45 +0600

    services

--- src/pages/Main/Service.js
+++ src/pages/Main/Service.js
@@ -2,217 +2,223 @@ import React from "react";
 
 const Service = () => {
   return (
-    <div class="bg-white py-6 sm:py-8 lg:py-12">
-      <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-        <div class="mb-10 md:mb-16">
-          <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
-            Our competitive advantage
+    <div
+      id="services"
+      class="section relative pt-20 pb-8 md:pt-16 md:pb-0 bg-white"
+    >
+      <div class="container xl:max-w-6xl mx-auto px-4">
+        <header class="text-center mx-auto mb-12 lg:px-20">
+          <h2 class="text-2xl leading-normal mb-2 font-bold text-black">
+            What We Do
           </h2>
-
-          <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
-            This is a section of some simple filler text, also known as
-            placeholder text. It shares some characteristics of a real written
-            text but is random or otherwise generated.
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 100 60"
+            style="margin: 0 auto;height: 35px;"
+            xml:space="preserve"
+          >
+            <circle
+              cx="50.1"
+              cy="30.4"
+              r="5"
+              class="stroke-primary"
+              style="fill: transparent;stroke-width: 2;stroke-miterlimit: 10;"
+            ></circle>
+            <line
+              x1="55.1"
+              y1="30.4"
+              x2="100"
+              y2="30.4"
+              class="stroke-primary"
+              style="stroke-width: 2;stroke-miterlimit: 10;"
+            ></line>
+            <line
+              x1="45.1"
+              y1="30.4"
+              x2="0"
+              y2="30.4"
+              class="stroke-primary"
+              style="stroke-width: 2;stroke-miterlimit: 10;"
+            ></line>
+          </svg>
+          <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">
+            Save time managing advertising &amp; Content for your business.
           </p>
-        </div>
+        </header>
 
-        <div class="grid gap-12 sm:grid-cols-2 xl:grid-cols-3 xl:gap-16">
-          <div class="flex flex-col items-center">
-            <div class="mb-6 flex h-12 w-12 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
-                />
-              </svg>
+        <div class="flex flex-wrap flex-row -mx-4 text-center">
+          <div
+            class="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp"
+            data-wow-duration="1s"
+            style="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;"
+          >
+            <div class="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+              <div class="inline-block text-gray-900 mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2rem"
+                  height="2rem"
+                  fill="currentColor"
+                  class="bi bi-search"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
+                </svg>
+              </div>
+              <h3 class="text-lg leading-normal mb-2 font-semibold text-black">
+                SEO Services
+              </h3>
+              <p class="text-gray-500">
+                This is a wider card with supporting text below as a natural
+                content.
+              </p>
             </div>
-
-            <h3 class="mb-2 text-center text-lg font-semibold md:text-xl">
-              Growth
-            </h3>
-            <p class="mb-2 text-center text-gray-500">
-              Filler text is dummy text which has no meaning however looks very
-              similar to real text.
-            </p>
-            <a
-              href="#"
-              class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
-            >
-              More
-            </a>
           </div>
-
-          <div class="flex flex-col items-center">
-            <div class="mb-6 flex h-12 w-12 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                />
-              </svg>
+          <div
+            class="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp"
+            data-wow-duration="1s"
+            data-wow-delay=".1s"
+            style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeInUp;"
+          >
+            <div class="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+              <div class="inline-block text-gray-900 mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2rem"
+                  height="2rem"
+                  fill="currentColor"
+                  class="bi bi-chat-square-dots"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
+                  <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"></path>
+                </svg>
+              </div>
+              <h3 class="text-lg leading-normal mb-2 font-semibold text-black">
+                Social Content
+              </h3>
+              <p class="text-gray-500">
+                This is a wider card with supporting text below as a natural
+                content.
+              </p>
             </div>
-
-            <h3 class="mb-2 text-center text-lg font-semibold md:text-xl">
-              Security
-            </h3>
-            <p class="mb-2 text-center text-gray-500">
-              Filler text is dummy text which has no meaning however looks very
-              similar to real text.
-            </p>
-            <a
-              href="#"
-              class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
-            >
-              More
-            </a>
           </div>
-
-          <div class="flex flex-col items-center">
-            <div class="mb-6 flex h-12 w-12 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
-                />
-              </svg>
+          <div
+            class="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp"
+            data-wow-duration="1s"
+            data-wow-delay=".3s"
+            style="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeInUp;"
+          >
+            <div class="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+              <div class="inline-block text-gray-900 mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2rem"
+                  height="2rem"
+                  fill="currentColor"
+                  class="bi bi-badge-ad"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3.7 11l.47-1.542h2.004L6.644 11h1.261L5.901 5.001H4.513L2.5 11h1.2zm1.503-4.852l.734 2.426H4.416l.734-2.426h.053zm4.759.128c-1.059 0-1.753.765-1.753 2.043v.695c0 1.279.685 2.043 1.74 2.043.677 0 1.222-.33 1.367-.804h.057V11h1.138V4.685h-1.16v2.36h-.053c-.18-.475-.68-.77-1.336-.77zm.387.923c.58 0 1.002.44 1.002 1.138v.602c0 .76-.396 1.2-.984 1.2-.598 0-.972-.449-.972-1.248v-.453c0-.795.37-1.24.954-1.24z"></path>
+                  <path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"></path>
+                </svg>
+              </div>
+              <h3 class="text-lg leading-normal mb-2 font-semibold text-black">
+                Creative ads
+              </h3>
+              <p class="text-gray-500">
+                This is a wider card with supporting text below as a natural
+                content.
+              </p>
             </div>
-
-            <h3 class="mb-2 text-center text-lg font-semibold md:text-xl">
-              Cloud
-            </h3>
-            <p class="mb-2 text-center text-gray-500">
-              Filler text is dummy text which has no meaning however looks very
-              similar to real text.
-            </p>
-            <a
-              href="#"
-              class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
-            >
-              More
-            </a>
           </div>
-
-          <div class="flex flex-col items-center">
-            <div class="mb-6 flex h-12 w-12 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M13 10V3L4 14h7v7l9-11h-7z"
-                />
-              </svg>
+          <div
+            class="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp"
+            data-wow-duration="1s"
+            style="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;"
+          >
+            <div class="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+              <div class="inline-block text-gray-900 mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2rem"
+                  height="2rem"
+                  fill="currentColor"
+                  class="bi bi-card-checklist"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"></path>
+                  <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"></path>
+                </svg>
+              </div>
+              <h3 class="text-lg leading-normal mb-2 font-semibold text-black">
+                Brand Identity
+              </h3>
+              <p class="text-gray-500">
+                This is a wider card with supporting text below as a natural
+                content.
+              </p>
             </div>
-
-            <h3 class="mb-2 text-center text-lg font-semibold md:text-xl">
-              Speed
-            </h3>
-            <p class="mb-2 text-center text-gray-500">
-              Filler text is dummy text which has no meaning however looks very
-              similar to real text.
-            </p>
-            <a
-              href="#"
-              class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
-            >
-              More
-            </a>
           </div>
-
-          <div class="flex flex-col items-center">
-            <div class="mb-6 flex h-12 w-12 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
-                />
-              </svg>
+          <div
+            class="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp"
+            data-wow-duration="1s"
+            data-wow-delay=".1s"
+            style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeInUp;"
+          >
+            <div class="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+              <div class="inline-block text-gray-900 mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2rem"
+                  height="2rem"
+                  fill="currentColor"
+                  class="bi bi-wallet2"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"></path>
+                </svg>
+              </div>
+              <h3 class="text-lg leading-normal mb-2 font-semibold text-black">
+                Budget &amp; Marketing
+              </h3>
+              <p class="text-gray-500">
+                This is a wider card with supporting text below as a natural
+                content.
+              </p>
             </div>
-
-            <h3 class="mb-2 text-center text-lg font-semibold md:text-xl">
-              Support
-            </h3>
-            <p class="mb-2 text-center text-gray-500">
-              Filler text is dummy text which has no meaning however looks very
-              similar to real text.
-            </p>
-            <a
-              href="#"
-              class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
-            >
-              More
-            </a>
           </div>
-
-          <div class="flex flex-col items-center">
-            <div class="mb-6 flex h-12 w-12 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              </svg>
+          <div
+            class="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp"
+            data-wow-duration="1s"
+            data-wow-delay=".3s"
+            style="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeInUp;"
+          >
+            <div class="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+              <div class="inline-block text-gray-900 mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2rem"
+                  height="2rem"
+                  fill="currentColor"
+                  class="bi bi-funnel"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"></path>
+                </svg>
+              </div>
+              <h3 class="text-lg leading-normal mb-2 font-semibold text-black">
+                Optimize conversions
+              </h3>
+              <p class="text-gray-500">
+                This is a wider card with supporting text below as a natural
+                content.
+              </p>
             </div>
-
-            <h3 class="mb-2 text-center text-lg font-semibold md:text-xl">
-              Dark Mode
-            </h3>
-            <p class="mb-2 text-center text-gray-500">
-              Filler text is dummy text which has no meaning however looks very
-              similar to real text.
-            </p>
-            <a
-              href="#"
-              class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
-            >
-              More
-            </a>
           </div>
         </div>
       </div>
